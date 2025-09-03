5285ff7281872a70f2883279556e79f17471491f
Donovan Campbell <donovan@dlcentertainment.net>
2025-09-02 20:52:32 -0700

    updated user model

--- models/user.js
+++ models/user.js
@@ -9,6 +9,18 @@ const userSchema = new mongoose.Schema({
         type: String, 
         required: true,
     },
+    email: {
+        type: String, 
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
 })
 
 userSchema.set('toJSON', {
