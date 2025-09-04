beb9c939cadf84bbfc8933897089a70d36ffe03c
CalebDH31 <calebdh31@gmail.com>
2025-09-03 22:21:17 -0600

    services added

--- models/movie.js
+++ models/movie.js
@@ -1,5 +1,47 @@
 const mongoose = require('mongoose')
 
 const movieSchema = new mongoose.Schema({
+    imdbID: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    posterUrl: {
+        type: String,
+        default: null,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+    },
+    genre: {
+        type: String,
+    },
+    director: {
+        type: String,
+    },
+    runtimeMinutes: {
+        type: Number,
+    },
+    averageRating: {
+        type: Number,
+        default: 0,
+    },
+    totalReviews: {
+        type: Number,
+        default: 0,
+    },
+    plot: {
+        type: String,
+    }
+},
+{ timestamps: true }
+)
 
-})
+const Movie = mongoose.model('Movie', movieSchema)
+
+module.exports = Movie
