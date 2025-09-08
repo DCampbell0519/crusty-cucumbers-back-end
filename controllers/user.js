00ddd977ff8dd37c8e127b7a087caf3669c98a88
Donovan Campbell <donovan@dlcentertainment.net>
2025-09-07 18:02:54 -0700

    profile made, need testing

--- controllers/user.js
+++ controllers/user.js
@@ -1,40 +1,53 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/user.js')
-const verifyToken = require('../middleware/verify-token.js')
-
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const verifyToken = require("../middleware/verify-token.js");
 
 // Here we are protecting as route ensuring a user must be logged in to access any data
-router.get('/', verifyToken, async (req, res) => {
-    try {
-        const users = await User.find({}, 'username')
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const users = await User.find({}, "username");
 
-        res.json(users)
-    } catch (error) {
-        res.status(500).json({ error: error.message })
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/profile", verifyToken, async (req, res) => {
+  try {
+    console.log("testing testing");
+    const user = await User.findById(req.user._id);
+
+    console.log("User:", user);
+    if (!user) {
+      return res.status(403).json({ error: "User not found" });
     }
-})
+    res.json({ user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 // Here a user must be logged in and they can only access their own data
-router.get('/:userId', verifyToken, async (req, res) => {
-    try {
-        console.log(req.user)
-        if (req.user._id !== req.params.userId) {
-            return res.status(403).json({error: 'Unauthorized'})
-        }
-
-        const user = await User.findById(req.params.userId)
-
-        if (!user) {
-            return res.status(404).json({error: 'User not found'})
-        }
-
-        res.json({ user })
-
-    } catch (error) {
-        res.status(500).json({ error: error.message })
+// View other community profiles
+router.get("/:userId", verifyToken, async (req, res) => {
+  try {
+    // console.log(req.user)
+    if (req.user._id !== req.params.userId) {
+      return res.status(403).json({ error: "Unauthorized" });
     }
-})
 
+    const user = await User.findById(req.params.userId);
 
-module.exports = router
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json({ user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -2,13 +2,15 @@ const jwt = require('jsonwebtoken')
 
 function verifyToken (req, res, next) {
     try {
-    
+        // console.log('Authorization header:', req.headers.authorization)
         const token = req.headers.authorization.split(' ')[1] // ['Bearer', '<token>'][1]
+        // console.log('Token extracted:', token)
             
                 // if the token is valid and was signed with our secret key
                 // this will return whatever was in the paylod when this token
                 // was created
                 const decoded = jwt.verify(token, process.env.JWT_SECRET)
+                // console.log('decoded token:', decoded)
                 
                 req.user = decoded.payload // contains the user data that was stored on sign-in
         
