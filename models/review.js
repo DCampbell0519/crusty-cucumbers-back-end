9cf9f414688a94346eb9bf24fad910cf43e89492
CalebDH31 <calebdh31@gmail.com>
2025-09-08 21:21:09 -0600

    fixed typo

--- models/review.js
+++ models/review.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User ', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   movieId: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie', required: true },
   rating: { type: Number, min: 1, max: 10, required: true },
   reviewText: { type: String, required: true },
