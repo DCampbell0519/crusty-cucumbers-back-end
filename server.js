7233ed8b67bd809780a88be00346489dede932f8
DtxLcl.19 <bryan.dtx19@icloud.com>
2025-09-08 20:20:12 -0500

    bv new

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connection.on('connected', () => {
 })
 
 
-app.use(cors({origin: 'http://localhost:5173', credentials: true })) 
+app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], credentials: true })) 
 app.use(express.json()) 
 app.use(morgan('dev'))
 
