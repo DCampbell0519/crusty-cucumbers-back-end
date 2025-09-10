2300fc6e443c7e42e23cb1943df0179492ae7438
Donovan Campbell <donovan@dlcentertainment.net>
2025-09-09 18:07:23 -0700

    deployment bug

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connection.on('connected', () => {
 
 
 
-app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], credentials: true })) 
+app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://crusty-cucumbers.netlify.app/'], credentials: true })) 
 
 app.use(express.json()) 
 app.use(morgan('dev'))
